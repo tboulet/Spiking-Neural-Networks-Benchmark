0778194aee8f1784d42a2b3b190212b622122ffb
Guillaume LEVY <levyguillaume10@yahoo.fr>
2023-09-18 16:14:40 +0200

    Fix train with cifar sequential

--- snn_benchmark/train.py
+++ snn_benchmark/train.py
@@ -4,7 +4,7 @@ import torch
 import utils
 
 from data.base_datasets import SHD_dataloaders, SSC_dataloaders, GSC_dataloaders
-from data.cifar_dataset import cifar10_repeat_dataloaders, cifar10_dataloaders
+from data.cifar_dataset import cifar10_repeat_dataloaders, cifar10_line_dataloaders, cifar10_dataloaders
 from data.iris_dataset import IRIS_dataloaders
 from data.har_dataset import HAR_dataloaders
 
@@ -35,7 +35,7 @@ config_mapping = {
     "SNN_IRIS": (ConfigSNNIRIS, IRIS_dataloaders, None),
     "ANN_CIFAR10": (ConfigANNCIFAR10, cifar10_dataloaders, ANN),
     "SNN_CIFAR10": (ConfigSNNCIFAR10, cifar10_dataloaders, None),
-    "SNN_CIFAR10_LINE": (ConfigSNNCIFAR10LINE, cifar10_dataloaders, None),
+    "SNN_CIFAR10_LINE": (ConfigSNNCIFAR10LINE, cifar10_line_dataloaders, None),
     "SNN_REPEAT_CIFAR10": (ConfigSNNREPEATCIFAR10, cifar10_repeat_dataloaders, None),
     "SNN_HAR": (ConfigHAR, HAR_dataloaders, None),
     # "CONV1D_HAR": (Config, HAR_dataloaders, TimeSeriesCNN),
@@ -45,9 +45,10 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"\n=====> Device = {device} \n\n")
 
 
-def launch(config_name='CONV1D_HAR'):
+def launch(config_name='SNN_HAR'):
     print(f"Launching with configuration : {config_name}")
-
+    if config_name not in config_mapping:
+        print(f"Config {config_name} does not exist. Using default config")
     config, dataloaders, model = config_mapping.get(config_name, (ConfigHAR, HAR_dataloaders, None))
     train_loader, valid_loader, test_loader = dataloaders(config)
 
